0c893868a12f433d6a7ff84fd009a6a57057abdb
Arnaud Wissler <arnaud.wissler@gmail.com>
2018-12-11 12:12:29 +0100

    header déployé

--- Components/Screens/EquipesScreen.js
+++ Components/Screens/EquipesScreen.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 
 
 export default class EquipesScreen extends React.Component {
 
-  render() {
-    return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Equipes</Text>
-      </View>
-    );
+
+
+    render() {
+      return (
+
+            <View style={styles.container}>
+              <Header/>
+              <ScrollView>
+                <Text>Equipes</Text>
+              </ScrollView>
+            </View>
+      );
+    };
   };
-};
+
+  class Header extends React.Component {
+    render() {
+      return (
+        <View style={styles.header}>
+          <Text style={styles.headertitle}>Equipes</Text>
+        </View>
+      );
+    };
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    header: {
+      flex: 0.15,
+      backgroundColor: '#4B85EA',
+      width: '100%',
+    },
+    headertitle: {
+      color: "#FFFFFF",
+      textAlign: 'center',
+      fontSize: 25,
+      fontWeight: 'bold',
+      marginTop: 25
+    }
+  });
--- Components/Screens/RencontresScreen.js
+++ Components/Screens/RencontresScreen.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 
 
 export default class RencontresScreen extends React.Component {
 
-  render() {
-    return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Rencontres</Text>
-      </View>
-    );
+
+    render() {
+      return (
+
+            <View style={styles.container}>
+              <Header/>
+              <ScrollView>
+                <Text>Rencontres</Text>
+              </ScrollView>
+            </View>
+      );
+    };
   };
-};
+
+  class Header extends React.Component {
+    render() {
+      return (
+        <View style={styles.header}>
+          <Text style={styles.headertitle}>Rencontres</Text>
+        </View>
+      );
+    };
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    header: {
+      flex: 0.15,
+      backgroundColor: '#4B85EA',
+      width: '100%',
+    },
+    headertitle: {
+      color: "#FFFFFF",
+      textAlign: 'center',
+      fontSize: 25,
+      fontWeight: 'bold',
+      marginTop: 25
+    }
+  });
