b542892e69ccd3fe6229ba177f48dd1ea840c96c
Paul-emile Pietri <pepietri@gmail.com>
2018-12-18 16:28:23 +0100

    reducer favoris

--- App.js
+++ App.js
@@ -5,17 +5,19 @@ import Navigation from './Components/Navigation/Navigation';
 
 // import de mes Reducers
 import teamStats from './Components/Reducers/stats.reducer'
+import addFavoris from './Components/Reducers/favoris.reducer'
 
 // import de mes outils Redux
 import {Provider} from 'react-redux';
 import {createStore, combineReducers} from 'redux';
 
 // Création de mon Store
-const store = createStore(combineReducers({teamStats}));
+const store = createStore(combineReducers({teamStats, addFavoris}));
 
 export default class App extends React.Component {
   render() {
-    return (<Provider store={store}>
+    return (
+    <Provider store={store}>
       <Navigation/>
     </Provider>);
   }
