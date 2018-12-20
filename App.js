7859af1acbd02b446663ac76ee31e7d4b8b4c258
Arnaud Wissler <arnaud.wissler@gmail.com>
2018-12-20 16:23:28 +0100

    ajout favoris sur classement

--- App.js
+++ App.js
@@ -6,14 +6,13 @@ import Navigation from './Components/Navigation/Navigation';
 // import de mes Reducers
 import teamStats from './Components/Reducers/stats.reducer'
 import teamClassement from './Components/Reducers/classement.reducer'
-import addFavoris from './Components/Reducers/favoris.reducer'
+import favoris from './Components/Reducers/favoris.reducer'
 import match from './Components/Reducers/match.reducer'
 // import de mes outils Redux
 import {Provider} from 'react-redux';
 import {createStore, combineReducers} from 'redux';
-
 // Création de mon Store
-const store = createStore(combineReducers({teamStats, teamClassement, addFavoris, match}));
+const store = createStore(combineReducers({teamStats, teamClassement, favoris, match}));
 
 export default class App extends React.Component {
   render() {
